d11c4e2391e1829bdef0f6036712589a0093c6d3
Magnun A V F <magnunavf@gmail.com>
2019-10-13 12:34:41 -0300

    feat: Create helper methods to operate over secrets file
    - Also create secrets sample file.

---
+++ api/src/utils/jwt.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const _ = require('lodash');
+const fs = require('fs');
+
+module.exports.getRandomSecretKey = () => {
+    const secretsHash = getSecretsHash();
+    const possibleKeys = _.keys(secretsHash);
+
+    return _.sample(possibleKeys);
+}
+
+module.exports.getSecret = (secretKey) => {
+    const secretsHash = getSecretsHash();
+    const possibleKeys = _.keys(secretsHash);
+
+    if (!possibleKeys.includes(secretKey)) {
+        const message = `Key ${secretKey} not exists!`
+        console.log(message);
+        throw new Error(message);
+    }
+
+    return secretsHash[secretKey];
+}
+
+const getSecretsHash = () => {
+    const secretsFile = fs.readFileSync('secrets.json');
+
+    return JSON.parse(secretsFile);
+}
